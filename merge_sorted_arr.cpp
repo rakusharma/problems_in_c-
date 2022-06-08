9d6743d466e584c8df6077ee42dffab2c605084e
Ravi Kumar Sharma <rasharma@micron.com>
2022-06-08 14:31:57 +0530

    cpp: fixed corner cases in merge sorted array
    case like uneven len and out of order number such as
    [1][2,3] [2][5,7] [2,9,11][1,3,8,10,20]

--- merge_sorted_arr.cpp
+++ merge_sorted_arr.cpp
@@ -18,18 +18,7 @@ class Solution {
 		void find(int a, int b, int& mi, int& ma)
 		{
 
-#if 0
-			if (a < b) {
-				mi = a;
-				ma = b;
-			}
-			else {
-				mi = b;
-				ma = a;
-			}
-#endif
 			a < b ? ( mi = a,ma = b) : ( mi = b,ma = a);
-			//cout << a <<"  "<<"  " <<b <<" " <<mi <<" "<< ma <<"\n"; 
 		}
 		void merge_sorted_arr(vector<int>& a, vector<int>& b, vector<int>& merg)
 		{
@@ -41,20 +30,50 @@ class Solution {
 
 
 			int lastmax = 0;
-			for (; it1 != a.end(); ++it1, ++it2) {
+			//for (; it1 != a.end() || it2 != b.end(); ++it1, ++it2) {
+			while(1) {
 
+				/*handling cases where the first array or second array has 1 element*/
+				if (it1 == a.end()) {
+					print(merg);
+					vector<int>::iterator t = merg.end();
+					cout << *it2 <<" " <<*(t-1) <<"\n";
+					if (*it2 < *(t-1)){
+						int temp = *(t - 1 );
+						merg.pop_back();
+						merg.push_back(*it2);
+						merg.push_back(temp);
+					}
+					else {
+						merg.push_back(*it2);
+					}
+					goto loop;
+					//break;
+				}
+				if (it2 == b.end()) {
+					vector<int>::iterator t = merg.end();
+					if (*it1 < *(t-1)){
+						int temp = *(t - 1 );
+						merg.pop_back();
+						merg.push_back(*it1);
+						merg.push_back(temp);
+					}
+					else {
+						merg.push_back(*it1);
+					}
+					//break;
+					goto loop;
+				}
 				find(*it1, *it2, mi, ma);
-				//cout <<"min " <<mi <<"max "<<ma << "it1 " << *it1 << "it2 "<< *it2<< "\n";
-				//
 				if (!merg.empty()){
 					vector<int>::iterator t = merg.end();
-					cout << "*" << mi << " " << *(t - 1) << "\n";
+					//cout << "*" << mi << " " << *(t - 1) << "\n";
 					if (mi <  *(t - 1)) { 
 						int temp = *(t - 1 );
 						merg.pop_back();
-						print(merg);
+						//print(merg);
 						merg.push_back(mi);
-						print(merg);
+						//print(merg);
 						if (temp < ma)  {
 							merg.push_back(temp);
 							merg.push_back(ma);
@@ -64,28 +83,35 @@ class Solution {
 							merg.push_back(temp);
 						}
 
+
 					}
 					else {
-				merg.push_back(mi);
-				merg.push_back(ma);
+						merg.push_back(mi);
+						merg.push_back(ma);
 					}
 				}
 				else {
-				merg.push_back(mi);
-				merg.push_back(ma);
-				print(merg);
+					merg.push_back(mi);
+					merg.push_back(ma);
+					//print(merg);
 				}
+loop:
+				if (it1 != a.end()) ++it1;
+				if (it2 != b.end()) ++it2;
 
-					
+				if (it1 == a.end() && it2 == b.end())break;
+     
 			}
 
 		}
 };
 
-
 int main()
 {
 	vector<int> a({2,7,9,11,20}), b({1,4,6,8,12});
+	//vector<int> a({1,11}), b({8, 9, 12});
+	//vector<int> a({1,11}), b({8, 9, 10,22});
+	//vector<int> b({1,11}), a({8, 9, 10});
 	vector<int> t;
 	Solution s;
 	s.print(a);
