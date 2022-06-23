d682a1afa409188f0ef926fd2f81ff799573cb32
Ravi Kumar Sharma <rasharma@micron.com>
2022-06-23 19:50:03 +0530

    cpp: edge maps
    added example

--- edges.cpp
+++ edges.cpp
@@ -2,6 +2,23 @@
 #include<vector>
 #include<algorithm>
 
+/*
+ * tree  2->3->4 2->3->5->6
+ [ 2 3 4  ]
+ [ 3 2 5  ]
+ [ 4 2  ]
+ [ 5 3 6  ]
+ [ 6 5  ]
+ 3 5 6 
+ 2  4 
+ 3  2 4 
+ 3 5 6 
+ 4 2 
+ 5 3 2 4 
+ 5 6 
+ 6 5
+ */
+
 
 using namespace std;
 
